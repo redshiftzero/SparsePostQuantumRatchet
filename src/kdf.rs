46e387458d438b81a3485e26bf6bb44595e52073
Rolfe Schmidt <141083381+rolfe-signal@users.noreply.github.com>
2025-12-04 12:53:53 -0700

    Upgrade to libcrux 0.0.4, spqr version to 1.3.0.
    Co-authored-by: Graeme Connell <gram@signal.org>

--- src/kdf.rs
+++ src/kdf.rs
@@ -4,24 +4,16 @@
 #[hax_lib::opaque]
 #[hax_lib::ensures(|res| res.len() >= okm_len)]
 pub fn hkdf_to_vec(salt: &[u8], ikm: &[u8], info: &[u8], okm_len: usize) -> Vec<u8> {
-    if cfg!(feature = "proof") {
-        libcrux_hkdf::hkdf(libcrux_hkdf::Algorithm::Sha256, salt, ikm, info, okm_len)
-            .expect("all lengths should work for SHA256")
-    } else {
-        let mut out = vec![0u8; okm_len];
-        hkdf_to_slice(salt, ikm, info, &mut out);
-        out
-    }
+    let mut out = vec![0u8; okm_len];
+    hkdf_to_slice(salt, ikm, info, &mut out);
+    out
 }
 
 #[hax_lib::opaque]
 #[hax_lib::ensures(|_| future(okm).len() == okm.len())]
 pub fn hkdf_to_slice(salt: &[u8], ikm: &[u8], info: &[u8], okm: &mut [u8]) {
-    if cfg!(feature = "proof") {
-        okm.copy_from_slice(&hkdf_to_vec(salt, ikm, info, okm.len()));
-    } else {
-        hkdf::Hkdf::<sha2::Sha256>::new(Some(salt), ikm)
-            .expand(info, okm)
-            .expect("all lengths should work for SHA256");
-    }
+    hkdf::Hkdf::<sha2::Sha256>::new(Some(salt), ikm)
+        .expand(info, okm)
+        .expect("all lengths should work for SHA256");
+    
 }
